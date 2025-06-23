34cc52466328ee7fc3581184040a95b87c7a7ece
laggerboy <labibjbaee@gmail.com>
2025-06-23 21:49:26 +0300

    animated mobile nav added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+const hamMenu = document.getElementById("ham-menu");
+const mobileMenu = document.getElementById("mobile-menu");
 
+hamMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("max-h-0");
+  mobileMenu.classList.toggle("max-h-86");
+});
