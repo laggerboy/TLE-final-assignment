ac096deb03a248840328b4657e7be6191d6c652c
laggerboy <labibjbaee@gmail.com>
2025-06-24 03:51:55 +0300

    added shop section for shop referral. added benefits section.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
+// collapse menu script
 const hamMenu = document.getElementById("ham-menu");
 const mobileMenu = document.getElementById("mobile-menu");
 
 hamMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("max-h-0");
   mobileMenu.classList.toggle("max-h-86");
+});
+
+// carousel script
+
+const images = document.querySelectorAll(".carousel-images img");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+let current = 0;
+
+function showSlide(index) {
+  images.forEach((img, i) => {
+    img.classList.toggle("opacity-100", i === index);
+    img.classList.toggle("opacity-0", i !== index);
+  });
+}
+
+prevBtn.addEventListener("click", () => {
+  current = (current - 1 + images.length) % images.length;
+  showSlide(current);
+});
+
+nextBtn.addEventListener("click", () => {
+  current = (current + 1) % images.length;
+  showSlide(current);
 });
